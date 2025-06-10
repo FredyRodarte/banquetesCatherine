c37549bbba679158ce07d0ae87736829b35d22c4
FredyRodarte <socionm@gmail.com>
2025-06-10 17:43:20 -0600

    Correccion de plantilla y modificacion al index(pagina inicial)

---
+++ static/js/Script.js
@@ -0,0 +1,20 @@
+// Inicializar tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+    
+// Validación Bootstrap para formularios
+    (() => {
+        'use strict'
+        const forms = document.querySelectorAll('.needs-validation')
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+            }, false)
+        })
+        })();
